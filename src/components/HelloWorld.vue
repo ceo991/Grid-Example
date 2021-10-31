39b94d2c71eff34cde7117b6fb65bde6f2743fba
DESKTOP-EUKB5FE\ceo99 <33651801+ceo991@users.noreply.github.com>
2021-10-31 22:40:05 +0300

    Changes Made

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="hello">
-    <button style="height:80px; width:80px;" @click="generateGrid()">GENERATE GRİD</button>
+    <button style="height: 80px; width: 80px" @click="generateGrid()">
+      GENERATE GRİD
+    </button>
     <table>
-      <tr v-for="ir in rows" :key="ir" style="height:80px" >
-        <td v-for="ic in columns" :key="ic" style="width:80px" :id="convertToIndex(ic,ir)"><button style="height:80px; width:80px;">{{convertToIndex(ic,ir)}}</button></td>
+      <tr v-for="ir in rows" :key="ir" style="height: 80px">
+        <td
+          v-for="ic in columns"
+          :key="ic"
+          style="width: 80px"
+          :id="convertToIndex(ic, ir)"
+        >
+          <button style="height: 80px; width: 80px">
+            {{ convertToIndex(ic, ir) }}
+          </button>
+        </td>
       </tr>
     </table>
-
   </div>
 </template>
 
@@ -15,40 +25,37 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      rows:[],
-      columns:[],
+      rows: [],
+      columns: [],
       trSize: 3,
       tdSize: 3,
-      ir:0,
-      ic:0,
-      tableGenerated: false
-    }
+      ir: 0,
+      ic: 0,
+      tableGenerated: false,
+    };
   },
   methods: {
-    generateGrid(){
-
-      if(!this.tableGenerated){
-  
-        for(this.ir = 1; this.ir <= this.trSize; this.ir++) {
+    generateGrid() {
+      if (!this.tableGenerated) {
+        for (this.ir = 1; this.ir <= this.trSize; this.ir++) {
           this.rows.push(this.ir);
         }
-        for(this.ic = 1; this.ic <= this.tdSize; this.ic++) {
+        for (this.ic = 1; this.ic <= this.tdSize; this.ic++) {
           this.columns.push(this.ic);
         }
         this.tableGenerated = true;
-      }else{
+      } else {
         alert("Grid already generated !");
       }
     },
-    convertToIndex(num1, num2){
-
-        if(num2 == 1){
-          num1 += 0;
-        } else if(num2 > 1){
-          num1 += this.tdSize*(num2-1);
-        }
+    convertToIndex(num1, num2) {
+      if (num2 == 1) {
+        num1 += 0;
+      } else if (num2 > 1) {
+        num1 += this.tdSize * (num2 - 1);
+      }
       return num1;
-    }
+    },
   },
   /*mounted () {
     this.addTableElement();
@@ -58,7 +65,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-table, tr, td {
+table,
+tr,
+td {
   border: 1px solid black;
   border-collapse: collapse;
 }
